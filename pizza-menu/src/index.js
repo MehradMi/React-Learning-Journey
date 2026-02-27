933afe3dc01e1d0dd08625bdd3c728943e408fa0
MehradMi <165255856+MehradMi@users.noreply.github.com>
2026-02-27 11:28:18 +0330

    Adding some styling to index.js

--- pizza-menu/src/index.js
+++ pizza-menu/src/index.js
@@ -1,19 +1,69 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
+import "./index.css";
 
 function App() {
 	return (
-		<>
-			<Pizza />
-		</>
+		<div className="container">
+			<Header />
+			<Menu />
+			<Footer />
+		</div>
 	);
 }
 
-function Pizza() {
+const Header = () => {
+	const style = {
+	};
+
+	return (
+		<header className="header">
+			<h1 style={style}>Pizza Co.</h1>
+		</header>
+	);
+}
+
+const Menu = () => {
+	return (
+		<main className="menu">
+			<h2>Menu</h2>
+			<Pizza />
+			<Pizza />
+			<Pizza />
+			<Pizza />
+		</main>
+	);	
+}
+
+const Footer = () => {
+	const hour = new Date().getHours();
+	const openningHour = 12;
+	const closingHour  = 22;
+	const isOpen = hour >= openningHour && hour <= closingHour;
+	if (isOpen) {
+		return (
+			<div>
+				<footer className="footer">
+					We're currently open!
+				</footer>
+			</div>
+		);
+	} else {
+		return (
+			<div className="footer">
+				<footer>
+					Sorry we're closed!
+				</footer>
+			</div>
+		);
+	}
+}
+
+const Pizza = () => {
 	return (
 		<div>
 			<img src="pizzas/spinaci.jpg" alt="Pizza Spinaci"/>
-			<h2>Pizza Spinaci</h2>	
+			<h3>Pizza Spinaci</h3>	
 			<p>Tomato, mozarella, spinach, and ricotta cheese</p>	
 		</div>	
 	);
