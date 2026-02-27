6a228a8dce74385da082a732602768d3cb3302b3
MehradMi <165255856+MehradMi@users.noreply.github.com>
2026-02-27 13:07:49 +0330

    Made it more dynamic

--- pizza-menu/src/index.js
+++ pizza-menu/src/index.js
@@ -2,6 +2,51 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
 
+const pizzaData = [
+  {
+    name: "Focaccia",
+    ingredients: "Bread with italian olive oil and rosemary",
+    price: 6,
+    photo_src: "pizzas/focaccia.jpg",
+    soldOut: false,
+  },
+  {
+    name: "Pizza Margherita",
+    ingredients: "Tomato and mozarella",
+    price: 10,
+    photo_src: "pizzas/margherita.jpg",
+    soldOut: false,
+  },
+  {
+    name: "Pizza Spinaci",
+    ingredients: "Tomato, mozarella, spinach, and ricotta cheese",
+    price: 12,
+    photo_src: "pizzas/spinaci.jpg",
+    soldOut: false,
+  },
+  {
+    name: "Pizza Funghi",
+    ingredients: "Tomato, mozarella, mushrooms, and onion",
+    price: 12,
+    photo_src: "pizzas/funghi.jpg",
+    soldOut: false,
+  },
+  {
+    name: "Pizza Salamino",
+    ingredients: "Tomato, mozarella, and pepperoni",
+    price: 15,
+    photo_src: "pizzas/salamino.jpg",
+    soldOut: true,
+  },
+  {
+    name: "Pizza Prosciutto",
+    ingredients: "Tomato, mozarella, ham, aragula, and burrata cheese",
+    price: 18,
+    photo_src: "pizzas/prosciutto.jpg",
+    soldOut: false,
+  },
+];
+
 function App() {
 	return (
 		<div className="container">
@@ -27,18 +72,17 @@ const Menu = () => {
 	return (
 		<main className="menu">
 			<h2>Menu</h2>
-			<Pizza />
-			<Pizza />
-			<Pizza />
-			<Pizza />
+			<ul className="pizzas">
+				{ pizzaData.map( (pizza) => (<Pizza pizzaObj={pizza} />) ) }
+			</ul>
 		</main>
-	);	
+	);
 }
 
 const Footer = () => {
 	const hour = new Date().getHours();
 	const openningHour = 12;
-	const closingHour  = 22;
+	const closingHour = 22;
 	const isOpen = hour >= openningHour && hour <= closingHour;
 	if (isOpen) {
 		return (
@@ -59,13 +103,16 @@ const Footer = () => {
 	}
 }
 
-const Pizza = () => {
+const Pizza = (props) => {
 	return (
-		<div>
-			<img src="pizzas/spinaci.jpg" alt="Pizza Spinaci"/>
-			<h3>Pizza Spinaci</h3>	
-			<p>Tomato, mozarella, spinach, and ricotta cheese</p>	
-		</div>	
+		<li className="pizza">
+			<img src={props.pizzaObj.photo_src} alt={props.pizzaObj.name} />
+			<div>
+				<h3>{props.pizzaObj.name}</h3>
+				<p>{props.pizzaObj.ingredients}</p>
+				<span>{props.pizzaObj.price}</span>
+			</div>
+		</li>
 	);
 }
 
