5d2a5087f452c790788e07fc1de66126bf309a22
MehradMi <165255856+MehradMi@users.noreply.github.com>
2026-04-03 21:48:16 +0330

    changes

--- tic-tac-toe/src/App.jsx
+++ tic-tac-toe/src/App.jsx
@@ -1,5 +1,16 @@
-function App() {
+import Player from './components/Player.jsx';
 
+function App() {
+  return (
+    <main>
+      <div id="game-container">
+        <ol id="players">
+					<Player initialName="Player 1" symbol="X"></Player>
+					<Player initialName="Player 2" symbol="O"></Player>
+        </ol>
+      </div>
+    </main>
+  );
 }
 
 export default App;
---
+++ tic-tac-toe/src/components/Player.jsx
@@ -0,0 +1,27 @@
+import { useState } from "react";
+
+function Player({initialName, symbol}) {
+
+	const [isEditing, setIsEditing] = useState(false);
+	const handleEditClick = () => {
+		setIsEditing(() => !isEditing);
+	}
+
+	const [playerName, setPlayerName] = useState(initialName);
+	const handleChange = (event) => {
+		setPlayerName(() => event.target.value);
+	}
+	
+  return (
+    <li>
+      <span className="player">
+				{!isEditing && <span className="player-name">{playerName}</span> } 
+				{isEditing && <input type="text" defaultValue={playerName} required onChange={handleChange}></input>}
+        <span className="player-symbol">{symbol}</span>
+      </span>
+      <button onClick={handleEditClick}>{isEditing ? "Save" : "Edit"}</button>
+    </li>
+  );
+}
+
+export default Player;
